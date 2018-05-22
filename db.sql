dc16b01f87f652711f032653cf103e438d2add35
Henrik Andersson <henrik.qvarfordt@gmail.com>
2018-05-22 14:01:40 +0200

    new db

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 16, 2018 at 08:13 AM
+-- Generation Time: May 22, 2018 at 11:58 AM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.1.5
 
@@ -45,7 +45,7 @@ INSERT INTO `comments` (`commentID`, `entryID`, `content`, `createdBy`, `created
 (5, 2, 'Blogg2', 1, '2018-05-06 00:00:06'),
 (6, 1, 'Blogg3', 1, '2018-05-06 00:00:06'),
 (7, 3, 'Blogg4', 1, '2018-05-06 00:00:06'),
-(8, 2, 'Blogg5', 2, '0000-00-00 00:00:00'),
+(8, 5, 'Blogg5', 2, '0000-00-00 00:00:00'),
 (9, 2, 'Blogg6', 2, '2018-05-06 00:00:08'),
 (10, 4, 'Blogg7', 2, '2018-05-06 00:00:23');
 
@@ -68,11 +68,11 @@ CREATE TABLE `entries` (
 --
 
 INSERT INTO `entries` (`entryID`, `title`, `content`, `createdBy`, `createdAt`) VALUES
-(1, 'Hej1', 'Blogg1', 2, '0000-00-00 00:00:00'),
-(2, 'Hej2', 'Blogg2', 1, '0000-00-00 00:00:00'),
-(3, 'Hej3', 'Blogg3', 2, '2018-04-16 00:00:00'),
-(4, 'Hej4', 'Blogg4', 1, '2018-04-16 00:00:00'),
-(5, 'datumtest', 'testar datum', 1, '2018-05-15 13:35:22');
+(1, 'Hej1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ', 2, '0000-00-00 00:00:00'),
+(2, 'Hej2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 2', 1, '0000-00-00 00:00:00'),
+(3, 'Hej3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 3', 2, '2018-04-16 00:00:00'),
+(4, 'Hej4', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 4', 30, '2018-04-16 00:00:00'),
+(5, 'datumtest', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 5', 1, '2018-05-15 13:35:22');
 
 -- --------------------------------------------------------
 
@@ -93,8 +93,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`userID`, `username`, `password`, `createdAt`) VALUES
 (1, 'henrik', 'henrik', '2018-05-06 00:00:00'),
-(27, 'hei', '$2y$10$Zr0u/ekcewEy/tMkEwglweS5dDySsxOgLXPiYbidsasumoIEKs3yC', '2018-05-15 17:39:32'),
-(28, 'jesper', '$2y$10$/URusidGMuUB4eBkNyD2eO4zoQgaClS7a5045jiogMlU0upQun842', '2018-05-16 10:10:17');
+(2, 'hei', '$2y$10$Zr0u/ekcewEy/tMkEwglweS5dDySsxOgLXPiYbidsasumoIEKs3yC', '2018-05-15 17:39:32'),
+(28, 'jesper', '$2y$10$/URusidGMuUB4eBkNyD2eO4zoQgaClS7a5045jiogMlU0upQun842', '2018-05-16 10:10:17'),
+(29, 'henrik', '$2y$10$DNIpZo2q0O9Jo6m8/toANOJNDCc8LRcwesVehUMge9B9LY6Iqvf4K', '2018-05-16 12:50:30'),
+(30, 'elvis', '$2y$10$X8Lt3kmJtQGjvyfrXA0zgu/BnQ6ceuwtUtqFrxXoFCcjt.mJGk1a6', '2018-05-16 12:57:29'),
+(31, 'lasse', '$2y$10$M3XPW0Hji2nGMcF6bZDUxupjbhzu.8tJgcm0zN6omncvO9Pfe7pfG', '2018-05-16 13:39:59');
 
 --
 -- Indexes for dumped tables
@@ -136,7 +139,7 @@ ALTER TABLE `entries`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;COMMIT;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
